2aa99defd794751b7fba0a65d734f4cf9d41beab
kacey152 <110165967+kacey152@users.noreply.github.com>
2022-12-03 14:45:38 +0800

    small fix
    Kacey Chua

--- frontend/my-app/src/components/AccountHeader.js
+++ frontend/my-app/src/components/AccountHeader.js
@@ -1,12 +1,15 @@
 import Transrecord from "./Transrecord";
 const AccountHeader = (props)=>{
+    const handleDelete = () => {
 
+    }
+    
     return(
         <div>
 
             {props.account.map((trans) => 
                     <div>
-                        
+                        <button onClick={()=> handleDelete}>Delete Transaction</button>
                         <p>Type: {trans.type}</p>
                         <p>ID: {trans.id}</p>
                         <p>Balance: {trans.balance}</p>
