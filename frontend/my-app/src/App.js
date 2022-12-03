2c39822e216608a43595cb0a50cbec948ab52fb7
Samuel P <samuel_png10@hotmail.com>
2022-12-03 13:38:30 +0800

    [Samuel] Wrapped app with the user context provider

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import './utils/firebase';
+import { UserContextProvider } from './context/userContext';
 
 function App() {
+  return (
+    <UserContextProvider>
+      <Login />
+    </UserContextProvider>
+  );
   return (
     <div className="App">
       <header className="App-header">
