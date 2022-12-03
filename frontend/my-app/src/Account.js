2e5898e7ecf03052e1c01be7e48a73d45f230d18
kacey152 <110165967+kacey152@users.noreply.github.com>
2022-12-03 12:15:26 +0800

    added handleAddTrans method
    Kacey Chua

--- frontend/my-app/src/Account.js
+++ frontend/my-app/src/Account.js
@@ -1,10 +1,15 @@
 const Account = (props)=>{
     const accountList = props.accountList
+    const handleAddTrans = props.handleAddTrans
     return(
 
             accountList.map((account)=>{
                     return(
                         <>
+                            <Addtrans 
+                                accountId={accountId}
+                                handleAddTrans={()=> handleAddTrans(id, receipient, date, amount, comments)}
+                            />
                             <AccountHeader 
                                 accountType={account.type}
                                 accountId={account.id}
