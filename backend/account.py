dbd439aa0c0ab27a87d1938507b3b4177235944e
xinyiiwangg <xinyiiwangg@gmail.com>
2022-12-03 11:50:44 +0800

    updated syntax - xinyi

--- backend/account.py
+++ backend/account.py
@@ -3,14 +3,14 @@ from database import *
 class Account(db.Model):
     __tablename__ = 'accounts'
 
-    ID = db.Column(db.Integer, primary_key=True)
-    userID = db.Column(db.Integer, foreign_key = True)
-    account_type = db.Column(db.String(255), nullable=False)
-    account_balance = db.Column(db.Float, nullable=False)
+    AccountID = db.Column(db.Integer, primary_key=True)
+    UserID = db.Column(db.Integer, foreign_key = True)
+    AccountType = db.Column(db.String(255), nullable=False)
+    AccountBalance = db.Column(db.Float, nullable=False)
     
-    def __init__(self, ID, userID, account_type, account_balance):
-        self.ID = ID
-        self.userID = userID
-        self.account_type = account_type
-        self.account_balance = account_balance
+    def __init__(self, AccountID, userID, AccountType, AccountBalance):
+        self.AccountID = AccountID
+        self.UserID = UserID
+        self.AccountType = AccountType
+        self.AccountBalance = AccountBalance
 
