481ad6da36a217bf5d6e6fa3d7db12b0c743d04c
xinyiiwangg <xinyiiwangg@gmail.com>
2022-12-03 11:51:25 +0800

    draft for [2] and [3] -xinyi

--- backend/app.py
+++ backend/app.py
@@ -2,26 +2,37 @@ from flask import Flask
 from flask import Flask, redirect, url_for, render_template, request, session, jsonify, flash, current_app, make_response, abort
 # from flask_cors import CORS, cross_origin
 
-from database import app
-# from flask_cors import CORS
-from flask_api import status
-from flask_sqlalchemy import SQLAlchemy
-
 from user import User
 
 import os
 
+from account import Account
+
 app.config['CORS_HEADERS'] = 'Content-Type'
 db = SQLAlchemy(app)
 
-@app.route('/')
-def hello():
-	return jsonify({'text': "hello!"})
 
-@app.route('/getUserDetails/<user_id>', methods=['GET'])
-def getUserDetails(user_id):
-    us = User.query.filter_by(userID=user_id).first()
-    return jsonify({'userID': us.userID, 'userName': us.userName, 'role': us.role})
+
+#[2]
+@app.route('/AccountInformation/<user_id>', methods=['GET'])
+def AccountInformation(user_id: int):
+    account = Account.query.filter_by(userID=user_id)
+    return account #make schema maybe?
+
+#[3]
+@app.route('/Scheduled_Transactions/<user_id>', methods=['GET'])
+def Scheduled_Transactions(user_id:int):
+    account = Account.query.filter_by(userID=user_id)
+    
+
+
+    
+
+
+    
+
+
+
 
 
 app.secret_key = 'seed'
