48b71c31e549f74f5f16f9e8fce5e50aa3e47c4a
ElizabethLeeWH <94830154+ElizabethLeeWH@users.noreply.github.com>
2022-12-03 11:53:16 +0800

    trans record css html js

---
+++ frontend/my-app/src/Transrecord.js
@@ -0,0 +1,8 @@
+document.getElementById("top-up").addEventListener("click", function(){
+    console.log("hello");
+    window.location= "/frontend/topup.html";
+  })
+  
+  document.getElementById("withdraw").addEventListener("click", function(){
+    window.location= "/frontend/withdrawal.html";
+  })
