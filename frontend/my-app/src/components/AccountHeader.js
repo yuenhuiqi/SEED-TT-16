90d8e7431ad74760688b3005745916294000b2ab
kacey152 <110165967+kacey152@users.noreply.github.com>
2022-12-03 14:49:41 +0800

    added userid testing
    Kacey CHua

--- frontend/my-app/src/components/AccountHeader.js
+++ frontend/my-app/src/components/AccountHeader.js
@@ -1,15 +1,15 @@
 import Transrecord from "./Transrecord";
 const AccountHeader = (props)=>{
     const handleDelete = () => {
-
+        /* api call to delete transaction*/
     }
-    
+
     return(
         <div>
 
             {props.account.map((trans) => 
                     <div>
-                        <button onClick={()=> handleDelete}>Delete Transaction</button>
+                        <button onClick={()=> handleDelete(trans.id,props.account.userId)}>Delete Transaction</button>
                         <p>Type: {trans.type}</p>
                         <p>ID: {trans.id}</p>
                         <p>Balance: {trans.balance}</p>
