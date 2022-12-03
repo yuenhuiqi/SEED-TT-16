23b1041f97a29a0e6af166d7336ae403924cd4c7
Samuel P <samuel_png10@hotmail.com>
2022-12-03 12:22:09 +0800

    [Samuel] Add firebase config file and initialize app

--- backend_express/index.js
+++ backend_express/index.js
@@ -1,6 +1,16 @@
+var admin = require('firebase-admin');
+var bodyParser = require('body-parser');
+var serviceAccount = require('./fbAdminConfig.json');
 const express = require('express');
 
 const app = express();
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
